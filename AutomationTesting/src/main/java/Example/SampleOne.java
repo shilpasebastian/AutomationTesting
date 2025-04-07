c3a0715b9bd0efb1fcfdfea2cb66abb068c7fc9c
shilpa <shilpa@DESKTOP-9PUA9QM>
2025-04-07 21:24:09 +0530

    Sample Programs

---
+++ AutomationTesting/src/main/java/Example/SampleOne.java
@@ -0,0 +1,18 @@
+package Example;
+
+public class SampleOne {
+	int a=10;
+	int b=20;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SampleOne obj=new SampleOne();
+		obj.sum();
+
+	}
+	public void sum() {
+		int c=a+b;
+		System.out.println("Sum="+c);
+	}
+
+}
---
+++ AutomationTesting/src/main/java/Example/SampleTwo.java
@@ -0,0 +1,16 @@
+package Example;
+
+public class SampleTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SampleTwo obj=new SampleTwo();
+		obj.display();
+
+	}
+	
+	public void display() {
+		System.out.println("Have a Nice Day...");
+	}
+
+}
